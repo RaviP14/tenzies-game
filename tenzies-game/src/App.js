a8f8b02079ab296646f3195e81c75451a1cea6ff
Ravi <ravi.patel14@outlook.com>
2022-02-25 19:54:31 +0000

    Added code to get an array of random numbers for die.

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -3,6 +3,23 @@ import './styles/style.css'
 import Die from './components/Die'
 
 function App() {
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+  function allNewDice(min, max, dice) {
+    const array = []
+    for (let i = 0; i < dice; i++) {
+        const element = getRandomInt(min, max)
+        array.push(element)
+    }
+    return array;
+  }
+
   return (
     <main>
       <div className="border">
