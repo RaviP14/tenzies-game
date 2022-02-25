949d220fa8068da9427372acf8b234825325da33
Ravi <ravi.patel14@outlook.com>
2022-02-25 19:30:12 +0000

    Added Die to App.js & styled die.

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,9 +1,24 @@
-import "./styles/style.css"
+import React from 'react';
+import './styles/style.css'
+import Die from './components/Die'
 
 function App() {
   return (
     <main>
-      <div className="border"></div>
+      <div className="border">
+      <div className="DieContainer">
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+            </div>
+      </div>
     </main>
   )
 }
